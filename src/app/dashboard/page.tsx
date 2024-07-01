25c47cb1754120335d0603972eb58fb4e9cc8282
Altieris Souza <altieris.souza@infoworker.com.br>
2024-07-01 17:07:34 -0300

    Adicionando nova rota e alteração nos estilos

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -6,7 +6,19 @@ import {
   GlobalStyle,
   Container,
   StyledTexts,
+  GridContainer,
+  Card,
+  ContainerName,
+  ContainerNews,
 } from "./style";
+import Image from "next/image";
+import books from "../../assets/books 1.png";
+import test from "../../assets/test 1.png";
+import science from "../../assets/science 1.png";
+import invesment from "../../assets/invesment 1.png";
+import program from "../../assets/program 1.png";
+import calendar from "../../assets/calendar 1.png";
+import setting from "../../assets/setting 1.png";
 
 export default function DashboardPage() {
   return (
@@ -20,7 +32,7 @@ export default function DashboardPage() {
         <div
           style={{
             height: "200px",
-            width: "100%",
+            width: "60%",
             gap: "10px",
           }}
         >
@@ -30,17 +42,148 @@ export default function DashboardPage() {
           <div
             style={{
               height: "3px",
-              width: "70%",
+              width: "100%",
               backgroundColor: "#30a7a0",
               borderRadius: "8px",
             }}
           ></div>
-          <div></div>
+          <GridContainer>
+            <Card>
+              <div style={{ height: "140px", alignContent: "center" }}>
+                <Image
+                  src={books}
+                  alt="fghfd da Imagem"
+                  style={{
+                    width: "100%",
+                    borderTopLeftRadius: "8px",
+                    borderTopRightRadius: "8px",
+                    padding: "10px",
+                  }}
+                />
+              </div>
+
+              <ContainerName>
+                <p>Disciplinas e materiais</p>
+              </ContainerName>
+            </Card>
+
+            <Card>
+              <div style={{ height: "140px", alignContent: "center" }}>
+                <Image
+                  src={test}
+                  alt="fghfd da Imagem"
+                  style={{
+                    width: "100%",
+                    borderTopLeftRadius: "8px",
+                    borderTopRightRadius: "8px",
+                    padding: "10px",
+                  }}
+                />
+              </div>
+
+              <ContainerName>
+                <p>Notas</p>
+              </ContainerName>
+            </Card>
+
+            <Card>
+              <div style={{ height: "140px", alignContent: "center" }}>
+                <Image
+                  src={science}
+                  alt="fghfd da Imagem"
+                  style={{
+                    width: "100%",
+                    borderTopLeftRadius: "8px",
+                    borderTopRightRadius: "8px",
+                    padding: "10px",
+                  }}
+                />
+              </div>
+
+              <ContainerName>
+                <p>Presenças e faltas</p>
+              </ContainerName>
+            </Card>
+
+            <Card>
+              <div style={{ height: "140px", alignContent: "center" }}>
+                <Image
+                  src={invesment}
+                  alt="fghfd da Imagem"
+                  style={{
+                    width: "100%",
+                    borderTopLeftRadius: "8px",
+                    borderTopRightRadius: "8px",
+                    padding: "10px",
+                  }}
+                />
+              </div>
+
+              <ContainerName>
+                <p>Financeiro</p>
+              </ContainerName>
+            </Card>
+
+            <Card>
+              <div style={{ height: "140px", alignContent: "center" }}>
+                <Image
+                  src={program}
+                  alt="fghfd da Imagem"
+                  style={{
+                    width: "100%",
+                    borderTopLeftRadius: "8px",
+                    borderTopRightRadius: "8px",
+                    padding: "10px",
+                  }}
+                />
+              </div>
+
+              <ContainerName>
+                <p>Grade curricular</p>
+              </ContainerName>
+            </Card>
+
+            <Card>
+              <div style={{ height: "140px", alignContent: "center" }}>
+                <Image
+                  src={calendar}
+                  alt="fghfd da Imagem"
+                  style={{
+                    width: "100%",
+                    borderTopLeftRadius: "8px",
+                    borderTopRightRadius: "8px",
+                    padding: "10px",
+                  }}
+                />
+              </div>
+              <ContainerName>
+                <p>Calendário</p>
+              </ContainerName>
+            </Card>
+
+            <Card>
+              <div style={{ height: "140px", alignContent: "center" }}>
+                <Image
+                  src={setting}
+                  alt="fghfd da Imagem"
+                  style={{
+                    width: "100%",
+                    borderTopLeftRadius: "8px",
+                    borderTopRightRadius: "8px",
+                    padding: "10px",
+                  }}
+                />
+              </div>
+              <ContainerName>
+                <p>Configurações</p>
+              </ContainerName>
+            </Card>
+          </GridContainer>
         </div>
         <div
           style={{
             height: "200px",
-            width: "30%",
+            width: "35%",
           }}
         >
           <div>
@@ -54,7 +197,7 @@ export default function DashboardPage() {
               borderRadius: "8px",
             }}
           ></div>
-          <div></div>
+          <ContainerNews></ContainerNews>
         </div>
       </Container>
     </>
--- src/app/dashboard/style.ts
+++ src/app/dashboard/style.ts
@@ -22,7 +22,7 @@ export const ContainerNav = styled.div`
 `;
 
 export const ContainerImagem = styled.div`
-  height: 300px;
+  height: 200px;
   width: 100%;
   background-color: #30a7a0;
 `;
@@ -32,10 +32,61 @@ export const Container = styled.div`
   margin: auto;
   width: 90%;
   margin-top: 30px;
+  gap: 5%;
 `;
 
 export const StyledTexts = styled.p`
   color: #30a7a0;
   font-size: 20px;
   font-weight: 500;
+`;
+
+export const GridContainer = styled.div`
+  margin-top: 20px;
+  display: grid;
+  gap: 16px;
+  grid-template-columns: repeat(4, 1fr);
+
+  @media (max-width: 1200px) {
+    grid-template-columns: repeat(3, 1fr);
+  }
+
+  @media (max-width: 900px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+
+  @media (max-width: 600px) {
+    grid-template-columns: 1fr;
+  }
+`;
+
+export const Card = styled.div`
+  background-color: #f8f9fa;
+  border-radius: 8px;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+  border: 2px solid #30a7a0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const ContainerNews = styled.div`
+  background-color: #30a7a0;
+  width: 100%;
+  height: 380px;
+  margin-top: 20px;
+  border-radius: 8px;
+`;
+
+export const ContainerName = styled.div`
+  width: 100%;
+  background-color: #30a7a0;
+  color: white;
+  text-align: center;
+  border-bottom-left-radius: 5px;
+  border-bottom-right-radius: 5px;
+
+  p {
+    padding: 10px;
+  }
 `;
